d4efb386d7b873fc8e3359aaa6cfb3d9da2dfe23
amejid <abdelmejid89@gmail.com>
2023-09-06 13:32:31 +0300

    Fetch cabins data and display it in a table

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,12 +1,18 @@
 import Heading from '../ui/Heading.jsx';
 import Row from '../ui/Row.jsx';
+import CabinTable from '../features/cabins/CabinTable.jsx';
 
 const Cabins = () => {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 };
 
