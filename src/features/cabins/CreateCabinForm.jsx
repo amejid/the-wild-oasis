e107b0885568dbf48a43a06df71a524526861075
amejid <abdelmejid89@gmail.com>
2023-09-06 15:29:04 +0300

    Implement creating a cabin

---
+++ src/features/cabins/CreateCabinForm.jsx
@@ -0,0 +1,110 @@
+import Input from '../../ui/Input.jsx';
+import Form from '../../ui/Form.jsx';
+import Button from '../../ui/Button.jsx';
+import FileInput from '../../ui/FileInput.jsx';
+import Textarea from '../../ui/Textarea.jsx';
+import { useForm } from 'react-hook-form';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { createCabin } from '../../services/apiCabins.js';
+import toast from 'react-hot-toast';
+import FormRow from '../../ui/FormRow.jsx';
+
+function CreateCabinForm() {
+  const queryClient = useQueryClient();
+  const { register, handleSubmit, reset, getValues, formState } = useForm();
+
+  const { errors } = formState;
+
+  const { mutate, isLoading: isCreating } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success('New cabin successfully created');
+      queryClient.invalidateQueries({
+        queryKey: ['cabins'],
+      });
+      reset();
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  const onSubmit = (data) => {
+    mutate(data);
+  };
+
+  const onError = (errors) => {
+    console.log(errors);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
+      <FormRow label="Cabin name" error={errors?.name?.message}>
+        <Input
+          type="text"
+          id="name"
+          {...register('name', { required: 'This field is required' })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Maximum Capacity" error={errors?.maxCapacity?.message}>
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: { value: 1, message: 'Capacity should be at least 1' },
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Regular Price" error={errors?.regularPrice?.message}>
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register('regularPrice', { required: 'This field is required' })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Discount" error={errors?.discount?.message}>
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          {...register('discount', {
+            required: 'This field is required',
+            validate: (value) =>
+              value <= getValues().regularPrice ||
+              'Discount should be less than the regular price',
+          })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Description" error={errors?.description?.message}>
+        <Textarea
+          type="number"
+          id="description"
+          defaultValue=""
+          {...register('description', { required: 'This field is required' })}
+          disabled={isCreating}
+        />
+      </FormRow>
+
+      <FormRow label="Cabin photo" error={errors?.image?.message}>
+        <FileInput id="image" accept="image/*" disabled={isCreating} />
+      </FormRow>
+
+      <FormRow>
+        {/* type is an HTML attribute! */}
+        <Button variation="secondary" type="reset">
+          Cancel
+        </Button>
+        <Button disabled={isCreating}>Add cabin</Button>
+      </FormRow>
+    </Form>
+  );
+}
+
+export default CreateCabinForm;
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
 import Heading from '../ui/Heading.jsx';
 import Row from '../ui/Row.jsx';
 import CabinTable from '../features/cabins/CabinTable.jsx';
+import Button from '../ui/Button.jsx';
+import { useState } from 'react';
+import CreateCabinForm from '../features/cabins/CreateCabinForm.jsx';
 
 const Cabins = () => {
+  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -11,6 +15,10 @@ const Cabins = () => {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
