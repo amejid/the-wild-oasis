4cbe569744fa950d251f69fca2e709a4f0a198c6
amejid <abdelmejid89@gmail.com>
2023-09-07 14:59:14 +0300

    Refactor modal as a compound component

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,6 +1,10 @@
+import PropTypes from 'prop-types';
+import { cloneElement, createContext, useContext, useState } from 'react';
 import { createPortal } from 'react-dom';
 import { HiXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
+
+import { useOutsideClick } from '../hooks/useOutsideClick.js';
 
 const StyledModal = styled.div`
   position: fixed;
@@ -51,18 +55,55 @@ const Button = styled.button`
   }
 `;
 
-const Modal = ({ children, onClose }) => {
+const ModalContext = createContext();
+
+const Modal = ({ children }) => {
+  const [openName, setOpenName] = useState('');
+
+  const close = () => setOpenName('');
+  const open = (opensWindowName) => setOpenName(opensWindowName);
+
+  return (
+    <ModalContext.Provider value={{ openName, open, close }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+const Open = ({ children, opens: opensWindowName }) => {
+  const { open } = useContext(ModalContext);
+
+  return cloneElement(children, { onClick: () => open(opensWindowName) });
+};
+
+const Window = ({ children, name }) => {
+  const { openName, close } = useContext(ModalContext);
+  const ref = useOutsideClick(close);
+  if (name !== openName) return null;
+
   return createPortal(
     <Overlay>
-      <StyledModal>
-        <Button onClick={onClose}>
+      <StyledModal ref={ref}>
+        <Button onClick={close}>
           <HiXMark />
         </Button>
-        <div>{children}</div>
+        <div>{cloneElement(children, { onCloseModal: close })}</div>
       </StyledModal>
     </Overlay>,
     document.body,
   );
+};
+
+Modal.Open = Open;
+Modal.Window = Window;
+
+Modal.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+Window.propTypes = {
+  children: PropTypes.any.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
 export default Modal;
